3fb88eb6bc4a5d2b1409966f8e006198845fa190
Ike Addy <ikaddy05@gmail.com>
2024-02-09 10:39:49 -0500

    Created posts and post compents and added styling

---
+++ src/post/Post.jsx
@@ -0,0 +1,30 @@
+import './post.css';
+import computer from '../assets/computer.png';
+
+export default function Post() {
+  return (
+    <div className='post'>
+        <img 
+            className='postImage'
+            src={computer} 
+            alt="" 
+        />
+
+        <div className="postInfo">
+            <div className="postCategories">
+                <span className="postCategory">Soccer</span>
+                <span className="postCategory">Programming</span>
+            </div>
+            <span className="postTitle">
+                Lorem ipsum dolor sit amet
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className='postDescription'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Voluptas distinctio ut, dolore optio sunt commodi odit, numquam, 
+            modi eius tenetur debitis id magnam alias voluptate quod. 
+            Fugit odit non veritatis.</p>
+    </div>
+  );
+}
