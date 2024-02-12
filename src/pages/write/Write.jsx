b73057b0f013b98c06215d05eea927469224e73f
Ike Addy <ikaddy05@gmail.com>
2024-02-11 22:25:55 -0500

    Finished styling for write page Next is to work on user settings and components

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,15 +1,38 @@
 import './write.css'
+import computerPicture from '../../assets/computer.png'
 
 export default function Write() {
   return (
     <div className='write'>
+      <img
+        className='writeImage' 
+        src={computerPicture} 
+        alt="" 
+      />
         <form className='writeForm'>
             <div className="writeFormGroup">
                 <label htmlFor="fileInput">
-                  <i class="fa-solid fa-plus"></i>
+                  <i className="writeIcon fa-solid fa-plus" style={{cursor: 'pointer'}}></i>
                 </label>
-                <input type="file" id="fileInput" className='chooseFile'/>
-                <input type="text" placeholder='Title'/>
+                <input 
+                  type="file" 
+                  id="fileInput" 
+                  className='chooseFile'
+                />
+                <input 
+                  type="text" 
+                  placeholder='Title' 
+                  className='writeInput' 
+                  autoFocus={true}
+                />
+            </div>
+            <div className="writeFormGroup">
+              <textarea 
+                placeholder='Tell your story...' 
+                type='text'
+                className='writeInput writeText'
+              ></textarea>
+              <button className="writeSubmit">Publish</button>
             </div>
         </form>    
     </div>
